b8c670ef275da04f7d9fe6d085e7e6facfe9cd0a
Adham-Ahmed <adhamahmed1234321@gmail.com>
2022-07-19 23:38:01 +0200

    lab 2 rails submission

---
+++ Rails Lab 2/README.md
@@ -0,0 +1,12 @@
+# Ruby Programming (Rails) - Lab 4
+
+# Process
+```
+bundle install
+```
+```
+rails db:migrate
+```
+```
+rails server
+```
---
+++ Rails Lab 2/app/controllers/roles_controller.rb
@@ -0,0 +1,53 @@
+class RolesController < ApplicationController
+  before_action :set_role, only: %i[ show edit update destroy ]
+    @roles = Role.all
+  end
+
+  end
+
+    @role = Role.new
+  end
+    authorize! :edit, @role
+
+  end
+    @role = Role.new(role_params)
+    respond_to do |format|
+      if @role.save
+        format.html { redirect_to role_url(@role), notice: "Role was successfully created." }
+        format.json { render :show, status: :created, location: @role }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @role.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+    respond_to do |format|
+      if @role.update(role_params)
+        format.html { redirect_to role_url(@role), notice: "Role was successfully updated." }
+        format.json { render :show, status: :ok, location: @role }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @role.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @role.destroy
+
+    respond_to do |format|
+      format.html { redirect_to roles_url, notice: "Role was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_role
+      @role = Role.find(params[:id])
+    end
+
+    def role_params
+      params.require(:role).permit(:name, :description)
+    end
+end
